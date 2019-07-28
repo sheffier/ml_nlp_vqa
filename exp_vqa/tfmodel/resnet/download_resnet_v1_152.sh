a6e597c7c46d848290d17a4b9d16b4ce5487e378
Or Shahaf <or7shahaf@gmail.com>
2019-07-28 13:55:37 +0300

    moved snmn code and added docs

--- exp_vqa/tfmodel/resnet/download_resnet_v1_152.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget -O ./exp_vqa/tfmodel/resnet/resnet_v1_152.tfmodel https://people.eecs.berkeley.edu/~ronghang/projects/snmn/resnet/resnet_v1_152.tfmodel
--- util/nets/__init__.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-
-channel_mean = np.array([123.68, 116.779, 103.939], dtype=np.float32)
