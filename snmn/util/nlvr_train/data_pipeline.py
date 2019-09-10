3527365de529f484c6b3ef7c56274856e6509efb
erez sheffi <erez.sheffi@gmail.com>
2019-09-10 10:15:53 +0300

    Enable a dataset options the may improve performance

--- snmn/util/nlvr_train/data_pipeline.py
+++ snmn/util/nlvr_train/data_pipeline.py
@@ -62,32 +62,35 @@ def create_masked_dataset(file_list, batch_size, max_seq_length, max_predictions
             "image_id": tf.cast(example["image_id"], tf.int32),
             "img_features": example["img_features"]}
 
-    with tf.device('/cpu:0'):
-        # generate file list
-        dataset = tf.data.Dataset.from_tensor_slices(file_list)
+    option_no_order = tf.data.Options()
+    option_no_order.experimental_deterministic = False
 
-        dataset = dataset.shuffle(tf.size(file_list, out_type=tf.dtypes.int64))
+    # generate file list
+    dataset = tf.data.Dataset.from_tensor_slices(file_list)
+    dataset = dataset.with_options(option_no_order)
 
-        # `cycle_length` is the number of parallel files that get read.
-        threads = multiprocessing.cpu_count()
-        cycle_length = threads
+    dataset = dataset.shuffle(tf.size(file_list, out_type=tf.dtypes.int64))
 
-        dataset = dataset.interleave(tf.data.TFRecordDataset,
-                                     cycle_length=cycle_length,
-                                     block_length=1,
-                                     num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    # `cycle_length` is the number of parallel files that get read.
+    threads = multiprocessing.cpu_count()
+    cycle_length = threads
 
-        dataset = dataset.repeat(num_epochs)
+    dataset = dataset.interleave(tf.data.TFRecordDataset,
+                                 cycle_length=cycle_length,
+                                 block_length=1,
+                                 num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
-        if is_training is True:
-            dataset = dataset.shuffle((20*batch_size))
+    dataset = dataset.repeat(num_epochs)
 
-        dataset = dataset.map(_parse, num_parallel_calls=threads)
+    if is_training is True:
+        dataset = dataset.shuffle((20*batch_size))
 
-        # dataset = dataset.batch(batch_size=batch_size, drop_remainder=is_training)
-        dataset = dataset.batch(batch_size=batch_size, drop_remainder=True)
+    dataset = dataset.map(_parse, num_parallel_calls=threads)
 
-        dataset = dataset.prefetch(buffer_size=batch_size)
+    # dataset = dataset.batch(batch_size=batch_size, drop_remainder=is_training)
+    dataset = dataset.batch(batch_size=batch_size, drop_remainder=True)
+
+    dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
 
     return dataset
 
@@ -117,29 +120,32 @@ def create_dataset(file_pattern, batch_size, max_seq_length, max_predictions_per
             "image_id": tf.cast(example["image_id"], tf.int32),
             "img_features": example["img_features"]}
 
-    with tf.device('/cpu:0'):
-        # generate file list
-        dataset = tf.data.Dataset.list_files(file_pattern, shuffle=is_training)
+    option_no_order = tf.data.Options()
+    option_no_order.experimental_deterministic = False
 
-        # `cycle_length` is the number of parallel files that get read.
-        threads = multiprocessing.cpu_count()
-        cycle_length = min(threads, len(file_pattern))
+    # generate file list
+    dataset = tf.data.Dataset.list_files(file_pattern, shuffle=is_training)
+    dataset = dataset.with_options(option_no_order)
 
-        dataset = dataset.interleave(tf.data.TFRecordDataset,
-                                     cycle_length=cycle_length,
-                                     block_length=1,
-                                     num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    # `cycle_length` is the number of parallel files that get read.
+    threads = multiprocessing.cpu_count()
+    cycle_length = min(threads, len(file_pattern))
 
-        dataset = dataset.repeat(num_epochs)
+    dataset = dataset.interleave(tf.data.TFRecordDataset,
+                                 cycle_length=cycle_length,
+                                 block_length=1,
+                                 num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
-        if is_training is True:
-            dataset = dataset.shuffle((20*batch_size))
+    dataset = dataset.repeat(num_epochs)
 
-        dataset = dataset.map(_parse, num_parallel_calls=threads)
+    if is_training is True:
+        dataset = dataset.shuffle((20*batch_size))
 
-        dataset = dataset.batch(batch_size=batch_size, drop_remainder=True)
+    dataset = dataset.map(_parse, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
-        dataset = dataset.prefetch(buffer_size=batch_size)
+    dataset = dataset.batch(batch_size=batch_size, drop_remainder=True)
+
+    dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
 
     return dataset
 
