88c8fd2049fd0c2c13212f13191a7873c5fbc8e0
Or Shahaf <or7shahaf@gmail.com>
2019-09-12 10:07:36 +0300

    draft: exctract_nlvr_vocabulary.py

---
+++ utilities/exctract_nlvr_vocabulary.py
@@ -0,0 +1,30 @@
+import os
+import json
+from util.text_processing import tokenize
+
+NLVR2_DATA_DIR = '../nlvr2/data'
+OUTPUT_FILE = f'{__file__}/../snmn/exp_nlvr/data/vocabulary_nlvr.txt'
+
+
+def get_nlvr2_tokens():
+    tokens = set()
+    for sentence in get_nlvr2_sentences():
+        tokens.update(tokenize(sentence))
+
+    return tokens
+
+
+def get_nlvr2_sentences():
+    for split in ('train', 'dev', 'test1'):
+        with open(os.path.join(NLVR2_DATA_DIR, f'{split}.json'), 'r') as f:
+            for line in f.readlines():
+                if not line:
+                    continue
+                sample = json.loads(line)
+                # TODO? synset = ' '.split(sample['synset'])
+                yield sample['sentence']
+
+
+if __name__ == '__main__':
+    with open(OUTPUT_FILE, 'w') as f:
+        f.writelines(sorted(get_nlvr2_tokens()))
